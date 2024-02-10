78edafd99876864d440fda8ee28f8eed547c699d
Wes Le <88017350+basedIdiot@users.noreply.github.com>
2024-02-09 18:18:16 -0800

    Update main.py

--- big bot/main.py
+++ big bot/main.py
@@ -7,9 +7,18 @@ import ihs_bindings
 CLAW_PORT = 0
 ARM_PORT = 1
 BACK_TOPHAT = 2
+"""
+4176 bot
+ARM_STRAIGHT_UP = 1300
+ARM_STRAIGHT = 200
+ARM_DOWN = 40
 
-ARM_STRAIGHT_UP = 1283
-ARM_STRAIGHT = 160
+CLAW_OPEN = 986
+CLAW_CLOSED = 1968
+"""
+ARM_STRAIGHT_UP = 1300
+ARM_STRAIGHT = 200
+ARM_DOWN = 40
 
 CLAW_OPEN  = 986
 CLAW_CLOSED = 1968
@@ -76,7 +85,7 @@ def move_servo(port, end_position):
 	k.disable_servo(port)
 
 #square up
-def drive_to_line(left_speed, right_speed, left_sensor, right_sensor):
+def drive_to_line(left_speed, right_speed, left_sensor=left_front, right_sensor=right_front):
 	print(left_sensor(), right_sensor())
 	while (left_sensor() > BLACK and right_sensor() > BLACK):
 		drive(left_speed, right_speed)
@@ -94,21 +103,24 @@ def drive_to_line_white(left_speed, right_speed, left_sensor, right_sensor):
 	while(right_sensor() < BLACK):
 		drive(0, right_speed)
 
-def reset():
-	k.enable_servos()
-	move_servo_slowly(ARM_PORT, ARM_STRAIGHT_UP, 5)
-	move_servo(CLAW_PORT, CLAW_OPEN)
-	k.disable_servos()
-
 def line_follow(port, seconds):
-	wantedtime = seconds*1000
+	end_time = seconds*1000
 	start = k.seconds()
-	while (k.seconds() - start < wantedtime):
+	while (k.seconds() - start < end_time):
 		if (k.analog(port) < BLACK):
 			k.create_drive_direct(-250, -150)
 		if(k.analog(port) > BLACK):
 			k.create_drive_direct(-150, -250)
-
+def test_drag():
+	success = retry_connect(5)
+	if not success:
+		print("Failed to connect!!!")
+		return -1
+	move_servo_slowly(ARM_PORT, ARM_STRAIGHT, 5)
+	move_servo(CLAW_PORT, CLAW_CLOSED)
+	k.create_drive_direct(100, 100)
+	k.msleep(10000)
+	k.create_disconnect()
 def main():
 	success = retry_connect(5) ## connects to but, tries 5 times
 	if not success:
@@ -117,7 +129,17 @@ def main():
 
 	#drive_to_line(-50, -50, k.get_create_lcliff_amt, k.get_create_rcliff_amt)
 	start_time = k.seconds()
+	
+	#line up arm with free-standing structure
+	move_servo_slowly(ARM_PORT, ARM_STRAIGHT_UP, 5)
+	move_servo(CLAW_PORT, CLAW_OPEN)
+	ihs_bindings.encoder_turn_degrees_v2(100, -40)
+	drive_to_line(100, 100, left_side, right_side)
+	drive_to_line_white(100, 100, right_side, left_side)
+	#drive(-50, -50)
+	k.msleep(1000)
 	#drives to starting line not square up b/c square up makes the bot turn
+	"""
 	while (left_side() > BLACK and right_side() > BLACK):
 		drive(50, 50)
 	#drive to make sure both sensors are on the starting line
@@ -130,27 +152,32 @@ def main():
 	drive(50, 50)
 	k.msleep(1000)
 	#k.msleep(500) #orginally 1600
+	"""
 	k.create_stop()
 
 	#grab free standing structure
-	#move_servo_slowly(ARM_PORT, 672, 10)
-
 	move_servo_slowly(ARM_PORT, ARM_STRAIGHT, 3) ## lowers hand to grab Structure
 	k.msleep(1000)
 
 	move_servo(CLAW_PORT, CLAW_CLOSED) ## closes claw
 	k.enable_servos()
 	k.msleep(500)
+	drive(150, 150)
+	k.msleep(1000)
 
 	#drive_to_line(250, 250, left_front, right_front)
 	#ihs_bindings.encoder_drive_straight_cm(-100, 10)
 	#drive(250, 250) ##drive past line and out of box
 	#k.msleep(500)
 
-	#turn out of box
+	'''#turn out of box
 	ihs_bindings.encoder_turn_degrees_v2(100, -20)
 	drive(100, 100)
-	k.msleep(500)
+	k.msleep(500)'''
+
+	#drive to second line
+	drive_to_line(150, 150, left_side, right_side)
+	ihs_bindings.encoder_turn_degrees_v2(50, 180)
 
 	#turn to middle line
 	ihs_bindings.encoder_turn_degrees_v2(100, -5)
@@ -168,6 +195,5 @@ def main():
 	print(k.seconds() - start_time)
 	k.create_disconnect()
 	k.disable_servos()
-
-#reset()
 main()
+
