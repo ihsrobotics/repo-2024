f461b2e3aec29c9a5024699e83a553877c3cb732
daylin2 <122577630+daylin2@users.noreply.github.com>
2024-03-07 17:48:18 -0800

    Update releaseJannis.py

--- small bot/releaseJannis.py
+++ small bot/releaseJannis.py
@@ -11,46 +11,65 @@ from kipr_functions import *
 def setup():
     k.set_servo_position(BOOM_ARM, BOOM_ASTRONAUT_POS)
     k.set_servo_position(CLASP, CLASP_DOWN_POS)
-    k.msleep(1500)
-
-def main():
-    k.set_servo_position(BOOM_ARM, BOOM_LEFT_POS)
     k.msleep(500)
+
+def part1():
+    k.set_servo_position(BOOM_ARM, BOOM_LEFT_POS)
+    k.msleep(10)
 
     drive(500, 1500, 1300)
     brake()
-    k.msleep(150)
+    k.msleep(10)
 
+
+    print(':PP')
     while (k.analog(BOOM_TOPHAT) < BOOM_BLACK):
         k.mav(LEFT_WHEEL, 1500)
         k.mav(RIGHT_WHEEL, 1500)
+        print(k.analog(BOOM_TOPHAT))
     brake()
-    k.msleep(50)
+    k.msleep(10)
 
+    drive(500, 500, 200)
+
+    print('>:3333')
     while (k.analog(BOOM_TOPHAT) > BOOM_BLACK):
         k.mav(LEFT_WHEEL, 1500)
         k.mav(RIGHT_WHEEL, 1500)
+        print(k.analog(BOOM_TOPHAT))
     brake()
-    k.msleep(50)
+    k.msleep(10)
 
-    drive(750,750,100)
+    k.set_servo_position(BOOM_ARM, 1024)
+    print('arm should move to 880', k.analog(BOOM_TOPHAT))
+    k.msleep(100)
+    print('urhghhh', k.analog(BOOM_TOPHAT))
 
-    k.set_servo_position(BOOM_ARM, 1028)
+def part2():
+    brake()
+    k.msleep(500)
 
     while (k.analog(BOOM_TOPHAT) < BOOM_BLACK):
-        k.mav(LEFT_WHEEL, -1500)
+        k.mav(LEFT_WHEEL, -1000)
         k.mav(RIGHT_WHEEL, 1500)
     brake()
-    k.msleep(50)
-
-    # k.set_servo_position(BOOM_ARM, BOOM_LEFT_POS)
-
-
+    while (k.analog(BOOM_TOPHAT) > BOOM_BLACK):
+        k.mav(LEFT_WHEEL, -1000)
+        k.mav(RIGHT_WHEEL, 1500)
+    brake()
     
+    for i in range(10):
+        k.mav(LEFT_WHEEL, 1000)
+        k.msleep(50)
+        k.mav(RIGHT_WHEEL, 1000)
+        k.msleep(50)
+    brake()
 
 if __name__ == "__main__":
     k.enable_servos()
     setup()
-    # main()
+    # part1()
+    # part2()
+
     k.disable_servos()
     k.ao()
