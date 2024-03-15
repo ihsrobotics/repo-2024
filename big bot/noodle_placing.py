fe80c14bd1a3a4462aad4ee8f1fcec85500d61ec
Byron De Guzman <122577569+bryonrr@users.noreply.github.com>
2024-03-14 17:48:53 -0700

    Updated function to move from center line to the right middle pipe. Also began to configure values for placing the noodles on the middle tube.

--- big bot/noodle_placing.py
+++ big bot/noodle_placing.py
@@ -38,6 +38,8 @@ def first_pipe():
 
 def center_line_to_large():
     # # square up with line running through center and raise arm
+    move_servo_slowly(ARM_PORT, ARM_STRAIGHT_UP, 10)
+    move_servo(SIDE_SERVO_PORT, SIDE_RIGHT)
     drive_to_line(150, 150, left_side, right_side)
     # move off of center line
     drive(150, 150)
@@ -50,36 +52,39 @@ def center_line_to_large():
             k.create_drive_direct(250, 150)
     # once at the top right vertical tape, drive slightly forward to somewhat center the bot
     drive(150, 150)
-    k.msleep(500)
+    k.msleep(600)
     # rotate the bot to have the arm lower down outside of the black tape
-    drive(150, -150)
+    move_servo(SIDE_SERVO_PORT, 1028) # Adjusted from SENSOR_CENER
+    drive(-150, 150)
     k.msleep(700)
-    ao()
-    move_servo_slowly(ARM_PORT, 0, 20)
-    # wait to ensure that the arm is all the way down
-    k.msleep(1000)
+    ao()        
     # rotate the bot until the tophat port at the end of the arm is aligned with the trv tape
-    while (k.analog(ARM_TOPHAT_PORT) < SENSOR_BLACK):
-        drive(25, -25)
-    drive(-50, 50)
-    k.msleep(50)
-    drive(0, 0)
-    move_servo_slowly(ARM_PORT, ARM_STRAIGHT_UP, 10)
-    k.msleep(500)
-    while (left_front() > BLACK):
-        drive(-150, -150)
-    ihs_bindings.encoder_turn_degrees_v2(100, 2)
-    drive(-150, -150)
-    msleep(300)
-    drive(0, 0)
+    while (k.analog(SIDE_TOPHAT_PORT) < SENSOR_BLACK):
+        drive(-25, 25)
+    drive(50, -50)
+    k.msleep(200)
+    # drive(0, 0)
+    #move_servo_slowly(ARM_PORT, ARM_STRAIGHT_UP, 10)
+    # k.msleep(500)
+    # while (left_front() > BLACK):
+    #     drive(-150, -150)
+    # ihs_bindings.encoder_turn_degrees_v2(100, 2)
+    # drive(-150, -150)
+    # msleep(300)
+    # drive(0, 0)
 
 
 def large_pipe():
     move_servo(SIDE_SERVO_PORT, SIDE_LEFT)
+
+    # Grab the noodles from the rack HERE
+
     move_servo_slowly(ARM_PORT, 1150, 10) #orginally 792, also 1050
+    drive(-150, -150)
+    k.msleep(800)
     turn_180(True)
-    drive(-75, -75)
-    k.msleep(150)
+    drive(-75, -75) #drive to pipe
+    k.msleep(600) # YOU NEED TO INCREASE THIS NEXT MEETING PLEASE
     ao()
     #move_servo_slowly(ARM_PORT, 875, 5) 875 should be 792
     #move_servo(ARM_PORT, 875)
@@ -87,12 +92,21 @@ def large_pipe():
     #ao()
 	#move_servo_slowly(ARM_PORT, 790, 5)
     #ihs_bindings.encoder_turn_degrees_v2(15, -1)
-    drive(9, 9)
-    move_servo_slowly(ARM_PORT, 792, 10)
+    drive(15, 15) # Changed from (9, 9)
+    move_servo_slowly(ARM_PORT, 792, 10) #lowers arm on pipe
     drive(0, 0)
-    #move_servo(CLAW_PORT, CLAW_OPEN)
+    k.msleep(10)
+    drive(15, 15)
+    move_servo_slowly(ARM_PORT, 600, 10) #lowers arm lower to make 2nd noddle go on pipe
+    drive(0,0)
     k.msleep(500)
-    for i in range(5):
+    for i in range(5): #jitter to get noddle on pipe
+        drive(-100, 100)
+        k.msleep(100)
+        drive(100, -100)
+        k.msleep(100)
+    move_servo(CLAW_PORT, CLAW_OPEN)
+    for i in range(5): #jitter to get noodle off claw
         drive(-100, 100)
         k.msleep(100)
         drive(100, -100)
@@ -115,6 +129,7 @@ if __name__ == "__main__":
     retry_connect(5)
     k.enable_servos()
     start = k.seconds()
+    center_line_to_large()
     large_pipe()
     print(k.seconds() - start)
     cleanup()
