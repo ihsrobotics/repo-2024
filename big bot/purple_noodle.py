75e975785c11b9c46be8cc7d3164a3c510c6f5f3
Moneynewspaper <70782814+Aryan905@users.noreply.github.com>
2024-03-28 17:42:49 -0700

    Update purple_noodle.py

--- big bot/purple_noodle.py
+++ big bot/purple_noodle.py
@@ -29,10 +29,11 @@ def print_battery_info():
 	capacity = k.get_create_battery_capacity()
 	if (capacity == 0 or charge == 0 or charge > capacity):
 		print("Battery info is invalid, try again later")
-		return
+		return False
 	print("Capacity", capacity)
 	print("Charge", charge)
 	print("Percentage", charge / (capacity if capacity != 0 else 1) * 100)
+	return True
 
 def drive_to_line(left_speed, right_speed, left_sensor=left_front, right_sensor=right_front):
 	print (left_sensor(), right_sensor())
@@ -60,7 +61,6 @@ def move_servo(port, end_position):
 	k.enable_servo(port)
 	k.set_servo_position(port, end_position)
 	k.msleep(100)
-	k.disable_servo(port)
 
 def move_servo_slowly(port, end_position, delay=0):
 	k.enable_servo(port)
@@ -69,7 +69,7 @@ def move_servo_slowly(port, end_position, delay=0):
 	    return
 	elif delay == 0:
 	    k.set_servo_position(port, end_position)
-	    msleep(100)
+	    k.msleep(100)
 	    return
 	while k.get_servo_position(port) < end_position and k.get_servo_position(port)+5 < 2048:
 		position = k.get_servo_position(port)+5
@@ -83,27 +83,36 @@ def move_servo_slowly(port, end_position, delay=0):
 
 def go_to_switch():
     ihs_bindings.encoder_turn_degrees_v2(100, 180)
-    
-def line_follow(SENSOR):
-	print(k.analog(ROD_PORT))
-	while k.analog(ROD_PORT) <= ROD_TOPHAT_BLACK:
+def is_right_front_white():
+	return right_front() > BLACK
+def is_left_side_white():
+	return left_side() > BLACK
+def is_rod_white():
+	return k.analog(ROD_PORT) < ROD_TOPHAT_BLACK
+def line_follow(sensor_port, continue_condition = is_right_front_white):
+	while continue_condition():
 		print(k.analog(ROD_PORT))
-		if k.analog(SENSOR) <= SWEEPER_BLACK:
-			drive(-150, -50)
-		elif k.analog(SENSOR) >= SWEEPER_BLACK:
-			drive(-50,-150)
+		if k.analog(sensor_port) <= SWEEPER_BLACK:
+			drive(-25, -125)
+		elif k.analog(sensor_port) >= SWEEPER_BLACK:
+			drive(-125,-25)
 
 def main():
+	"""
 	success = retry_connect(5) ## connects to but, tries 5 times
 	if not success:
 		print("Failed to connect!!!")
 		return -1
-	print_battery_info()
+	"""
+	if not print_battery_info():
+		print("invalid battery info probably means bot will do something dumb")
+		return -1
 	start_time = k.seconds()
 
 	#line up arm with free-standing structure with middle rods
 	move_servo_slowly(ARM_PORT, ARM_STRAIGHT, 5) 
 	k.msleep(200)
+	
 	print("servo should've moved")
 	drive(-100, -100)
 	k.msleep(500)
@@ -134,32 +143,49 @@ def main():
 	drive_to_line_white(150,150, left_side, right_side)
 	drive(0,0)
 	
-	move_servo_slowly(ROD_PORT,ROD_STRAIGHT,5)
-	ihs_bindings.encoder_turn_degrees_v2(100,110)
+	move_servo(ROD_PORT,ROD_STRAIGHT)
+	ihs_bindings.encoder_turn_degrees_v2(100,95)
 	k.msleep(300)
 	
+	#turns toward switch
 	drive(-30,30)
-	while k.analog(ROD_TOPHAT)<= ROD_TOPHAT_BLACK:
-		print(k.analog(ROD_TOPHAT))
+	while k.analog(ROD_TOPHAT)  <= ROD_TOPHAT_BLACK:
 		pass
 	drive(0,0)
-	k.msleep(3000)
+	
 	move_servo_slowly(ROD_PORT,ROD_SIDE,5)
+	move_servo_slowly(ARM_PORT, ARM_DOWN, 5)
+	#drives to switch
 	line_follow(SWEEPER_TOPHAT_PORT)
+	drive(0,0)
+
+	#flips switch UP
+	#currently flips switch down
+	move_servo(ARM_PORT,ARM_MIDDLE_NOODLE)
+
+	ihs_bindings.encoder_turn_degrees_v2(100,170)
+	#drive(-300,-300)
+	#k.msleep(200)
+	line_follow(SWEEPER_TOPHAT_PORT, is_left_side_white)
+	drive(0, 0)
+	move_servo(ROD_PORT, ROD_STRAIGHT)
+	drive(30,-30)
+	while k.analog(ROD_TOPHAT) <= ROD_TOPHAT_BLACK:
+		pass
+	drive(0,0)
+
 	
-
-
-
-
-
-    
-	
-
-	
-	
-
 retry_connect(5)
-line_follow(SWEEPER_TOPHAT_PORT)
-#drive_to_line_white(150,150, left_side, right_side)
+print_battery_info()
+#line_follow(SWEEPER_TOPHAT_PORT)
+#move_servo_slowly(ARM_PORT,ARM_DOWN)
 k.enable_servos()
-#main()
+"""
+while True:
+	k.msleep(1000)
+	print(right_front())
+"""
+#drive(0,0)
+#drive_to_line_white(150,150, left_side, right_side)
+main()
+#ihs_bindings.encoder_turn_degrees_v2(100,180)
