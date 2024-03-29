97cdf63a0f939806c81c3d2cb36cf786a6a3357d
daylin2 <122577630+daylin2@users.noreply.github.com>
2024-03-28 17:42:02 -0700

    Update moon_station_astronaut.py

--- small bot/moon_station_astronaut.py
+++ small bot/moon_station_astronaut.py
@@ -6,70 +6,42 @@ from config_loader import *
 # set up on left side of the horizontal line from the solar panel facing west
 
 def go_to_middle():
-    while not on_tape(BOOM_TOPHAT, BOOM_BLACK):
-        line_follow(1000, 1500, "LEFT")
-    brake()
-
-def drop_stuff():
-    while not on_tape(BOOM_TOPHAT, BOOM_BLACK):
-        drive(1500, -1500)
-    brake()
-    while on_tape(BOOM_TOPHAT, BOOM_BLACK):
-        drive(1500, -1500)
-    brake()
-
-    k.set_servo_position(BOOM_SERVO, 1500)
+    set_arm_pos(450)
+    k.set_servo_position(BOOM_SERVO, BOOM_RIGHT_POS)
     k.msleep(100)
 
-    while on_tape(BOOM_TOPHAT, BOOM_BLACK):
-        drive(-500, -1200)
-    brake()
-    while not on_tape(BOOM_TOPHAT, BOOM_BLACK):
-        drive(-500, -1200)
-    brake()
-
-    print(f'bruh {k.analog(FRONT_TOPHAT)}')
-
-    while on_tape(FRONT_TOPHAT, FRONT_BLACK):
-        drive(-1000, -1000)
-    brake()
-
-    drive(-300, 300, 200)
-
-    print(f':3 {k.analog(FRONT_TOPHAT)}')
-
-
-    while on_tape(FRONT_TOPHAT, FRONT_BLACK):
-        drive(1000, 1000)
-    brake()
     while not on_tape(FRONT_TOPHAT, FRONT_BLACK):
-        drive(1000, 1000)
+        line_follow(1200, 1500, "RIGHT", BOOM_TOPHAT, BOOM_BLACK)
     brake()
+
     while on_tape(FRONT_TOPHAT, FRONT_BLACK):
-        drive(1000, 1000)
+        drive(1500,1500)
     brake()
-    print(f':P {k.analog(BOOM_TOPHAT)}')
 
+    drive(1000, 1000, 550)
+    brake()
 
-    # while on_tape(BOOM_TOPHAT, BOOM_BLACK):
-    #     drive(500, -500)
-    # brake()
-    # while not on_tape(BOOM_TOPHAT, BOOM_BLACK):
-    #     drive(500, -500)
-    # brake()
+    k.set_servo_position(BOOM_SERVO, 900)
+    k.msleep(100)
+
+    while not on_tape(BOOM_TOPHAT, BOOM_BLACK):
+        drive(570, -525)
+    brake()
+
+def drop_em():
+    k.msleep(700)
+    
+    k.mav(ARM, -500)
+    k.msleep(500)
 
 def main():
-    # set_arm_pos(340)
-    k.set_servo_position(BOOM_SERVO, BOOM_LEFT_POS)
-    # k.set_servo_position(LEVER_SERVO, 2047)
-    k.msleep(100)
-
     go_to_middle()
-    drop_stuff()
+    drop_em()
+
 
 
 if __name__ == "__main__":
     k.enable_servos()
     main()
-    # print(k.get_servo_position(BOOM_SERVO))
+
     k.disable_servos()
