66cc44bd322c7101a1ccc4f318321f94cd6209cf
yepfrosty <115395247+yepfrosty@users.noreply.github.com>
2024-04-04 13:48:38 -0700

    Update jannis_dropoff.py

--- small bot/jannis_dropoff.py
+++ small bot/jannis_dropoff.py
@@ -5,27 +5,34 @@ from config_loader import *
 
 
 
+
+
 def main():
     #Back into black tape
     while k.analog(FRONT_TOPHAT) < BLACK:
         drive(-500, -500)
     brake()
 
-    #Line follow for 4.725 seconds to bring jannis under the tube
+    #Line follow for  seconds to bring jannis under the tube
     start = k.seconds()
-    while k.seconds() < start + 5000:
+    while k.seconds() < start + 5100:
         line_follow(500, 700, "RIGHT", BOOM_BLACK, BOOM_TOPHAT)
     brake()
 
     #Move the left wheel back to straighten out jannis
     drive(-200, 0)
-    k.msleep(700)
+    k.msleep(1350)
     brake()
 
     #Release Jannis
     for i in range (k.get_servo_position(JANNIS), 2047):
         k.set_servo_position(JANNIS, i)
         k.msleep(1)
+    
+    #Move Forward
+    drive(500, 500)
+    k.msleep(500)
+    brake()
     
     
 
@@ -37,5 +44,3 @@ if __name__ == "__main__":
     k.set_servo_position(LEVER_SERVO, 2047)
 
 main()
-
-
