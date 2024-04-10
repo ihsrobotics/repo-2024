1d870c6115be569b4302259c4702c7fe3b2f0784
daylin2 <122577630+daylin2@users.noreply.github.com>
2024-04-10 15:47:10 -0700

    Update moon_station_astronaut.py

--- small bot/moon_station_astronaut.py
+++ small bot/moon_station_astronaut.py
@@ -5,38 +5,86 @@ from config_loader import *
 
 # set up on left side of the horizontal line from the solar panel facing west
 
-def go_to_middle():
-    set_arm_pos(450)
-    k.set_servo_position(BOOM_SERVO, BOOM_RIGHT_POS)
-    k.msleep(100)
+def go_to_station():
+    k.set_servo_position(BOOM_SERVO, 630)
+    k.msleep(150)
 
     while not on_tape(FRONT_TOPHAT, FRONT_BLACK):
-        line_follow(1200, 1500, "RIGHT", BOOM_TOPHAT, BOOM_BLACK)
+        line_follow(1000, 1300, "LEFT", BOOM_TOPHAT, BOOM_BLACK)
     brake()
 
     while on_tape(FRONT_TOPHAT, FRONT_BLACK):
-        drive(1500,1500)
+        drive(500,500)
     brake()
 
-    drive(1000, 1000, 550)
-    brake()
-
-    k.set_servo_position(BOOM_SERVO, 900)
-    k.msleep(100)
+def turn_to_station():
+    k.set_servo_position(BOOM_SERVO, 750)
+    k.msleep(200)
 
     while not on_tape(BOOM_TOPHAT, BOOM_BLACK):
-        drive(570, -525)
+        drive(1500, -300)
     brake()
 
-def drop_em():
-    k.msleep(700)
+    k.set_servo_position(BOOM_SERVO, BOOM_RIGHT_POS)
+    k.msleep(300)
+
+    print(k.analog(BOOM_TOPHAT))
+
+    while not on_tape(BOOM_TOPHAT, BOOM_BLACK):
+        drive(-1100, -1500)
+    brake()
+
+
+def adjust():
+    set_arm_pos(80)
+
+    while on_tape(BOOM_TOPHAT, BOOM_BLACK):
+        drive(500, 500)
+    brake()
+    print('1) ', k.analog(BOOM_TOPHAT))
+
+    while not on_tape(BOOM_TOPHAT, BOOM_BLACK):
+        drive(-100, -1000)
+    brake()
+    k.msleep(150)
+    print('2) ', k.analog(BOOM_TOPHAT))
+    while on_tape(BOOM_TOPHAT, BOOM_BLACK):
+        drive(-50, -500)
+    brake()
+    print('3) ', k.analog(BOOM_TOPHAT))
+
+
+    print('4) ', k.analog(FRONT_TOPHAT))
+
+    while not on_tape(FRONT_TOPHAT, FRONT_BLACK):
+        drive(500, 500)
+    brake()
+    print('5) ', k.analog(FRONT_TOPHAT))
+
+    drive(-500, -500, 180)
     
-    k.mav(ARM, -500)
-    k.msleep(500)
+def drop():
+    brake()
+    k.msleep(100)
+
+    k.mav(ARM, -200)
+    k.msleep(810)
+
+    k.mav(ARM, 0)
+    k.mav(10)
 
 def main():
-    go_to_middle()
-    drop_em()
+    set_arm_pos(600)
+
+    go_to_station()
+    turn_to_station()
+    adjust()
+    drop()
+
+    brake()
+    k.msleep(800)
+
+    drive(-500, -500, 1500)
 
 
 
