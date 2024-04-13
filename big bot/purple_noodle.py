038e37bdf224ce5a5c4b803718355d5c6c2d856d
Moneynewspaper <70782814+Aryan905@users.noreply.github.com>
2024-04-12 18:18:50 -0700

    Update purple_noodle.py

--- big bot/purple_noodle.py
+++ big bot/purple_noodle.py
@@ -5,7 +5,6 @@ k = CDLL(kipr)
 import ihs_bindings
 import sys
 import math
-import time
 
 sys.path.append("/home/pi/Documents/IME_files/purpleNoodle/include")
 from roomba_drive import *
@@ -52,8 +51,10 @@ def is_right_front_white():
 	return right_front() > BLACK
 def is_left_side_white():
 	return left_side() > BLACK
+def is_right_side_white():
+	return right_side() > BLACK
 def is_rod_white():
-	return k.analog(ROD_PORT) < ROD_TOPHAT_BLACK
+	return k.analog(ROD_PORT) > ROD_TOPHAT_BLACK
 def sweep():
 	speed = -1000
 	k.mav(SWEEPER_PORT, speed)
@@ -85,7 +86,7 @@ def main():
 	#drive_to_line_white(300, 300, left_side, right_side)
 	k.msleep(100)
 
-	start_time = time.time()
+	start_time = time()
 	drive(500, 500) #orig speed: 150, 150
 	k.msleep(1100) #orig time: 1200
 	ihs_bindings.encoder_turn_degrees_v2(500, -90) #orig speed: 50, 90 deg clockwise
@@ -100,7 +101,7 @@ def main():
 	ihs_bindings.encoder_turn_degrees_v2(500,90)
 	#sweep()
 	drive_to_line(300, 300, left_side, right_side)
-	align_start = time.time()
+	align_start = time()
 	drive_to_line_white(150,150, left_side, right_side)
 	drive(0,0)
 
@@ -111,25 +112,38 @@ def main():
 	sweeper_align_black(30, -30)
 	move_servo(ROD_PORT, 1800) #position rod so line follow is precise, og: 1285
 	
-	"""
+	
 	def is_rod_black():
 		return k.analog(ROD_TOPHAT) < ROD_TOPHAT_BLACK
-	line_follow(SWEEPER_TOPHAT_PORT, is_rod_black)
-	"""
-	#does the same thing as the line
+	#line_follow(SWEEPER_TOPHAT_PORT, is_rod_black)
 	
+	#does the same thing as the line
 
-	#line_follow(SWEEPER_TOPHAT_PORT, lambda : right_side() < BLACK)
+	line_follow(SWEEPER_TOPHAT_PORT, is_rod_black)
+	drive(0,0)
+	'''
 	drive(-200,-200)
+	
 	while right_side() > BLACK:
 		pass
 	drive(0,0)
-    #pick up cubes
+	move_servo_slowly(ARM_PORT, ARM_SWITCH_UP,10)
+	
 	drive(100,100)
+	k.msleep(150)
+
+
+    
+    #pick up cubes
+	drive(200,200)
 	k.msleep(100)
+
+
+
 	drive(0,0)
-	move_servo_slowly(ARM_PORT,ARM_SHELF)
-	move_servo_slowly(CLAW_PORT, CLAW_CLOSED, 5)
+	move_servo_slowly(ARM_PORT,ARM_SHELF-10,10)
+	k.msleep(100)
+	move_servo_slowly(CLAW_PORT, CLAW_CLOSED, 10)
 	k.msleep(500)
 	move_servo_slowly(ARM_PORT, ARM_STRAIGHT_UP, 5)
 	
@@ -138,15 +152,16 @@ def main():
 	move_servo_slowly(ARM_PORT, ARM_DOWN,5)
 	move_servo(CLAW_PORT, CLAW_OPEN)
 	ihs_bindings.encoder_turn_degrees_v2(500, 130)
-	#drive(-100,-100)
-	#k.msleep(250)
+	move_servo_slowly(ARM_PORT, ARM_STRAIGHT_UP, 5)
+	line_follow(SWEEPER_TOPHAT_PORT, is_rod_black)
+	line_follow(SWEEPER_TOPHAT_PORT, is_rod_white)
 	print("done")
-	
+	'''
 
 	#turn towards drawer
-	'''
+	
 	ihs_bindings.encoder_turn_degrees_v2(500, -33)
-    
+	
 	move_servo(CLAW_PORT, CLAW_CLOSED)
 
 	move_servo_slowly(ARM_PORT, ARM_DOWN-20, 5)
@@ -155,46 +170,105 @@ def main():
 
 
 	move_servo(CLAW_PORT, CLAW_DRAWER)
+	#drive into handle
 	drive(100,100)
 	while right_side() < BLACK:
 		pass
-	k.msleep(155)
+	k.msleep(200)#0g: 170
 	drive(0, 0)
-	
-	ihs_bindings.encoder_turn_degrees_v2(100,-5)
-	ihs_bindings.encoder_turn_degrees_v2(100,3)
+	#re adjust
+	ihs_bindings.encoder_turn_degrees_v2(100,-3)
+	ihs_bindings.encoder_turn_degrees_v2(100,2)
 	
 
-	k.msleep(1000)
+	#k.msleep(1000)
 
-	ihs_bindings.encoder_turn_degrees_v2(100,-7)
+	ihs_bindings.encoder_turn_degrees_v2(50,-5)
 	drive(-100,-100)
 	k.msleep(300)
 	drive(0,0)
 	
-	k.msleep(1000)
+	#k.msleep(1000)
+	#re adjust
+	move_servo_slowly(CLAW_PORT, CLAW_DRAWER + 15)
+	ihs_bindings.encoder_turn_degrees_v2(50, -15)
+	#release drawer
+	drive(-200,-200)
+	k.msleep(350)
+	drive(0,0)
+	move_servo_slowly(ARM_PORT,ARM_STRAIGHT_UP, 10)
+	#ihs_bindings.encoder_turn_degrees_v2(100,45)
+	#drive back 
+	drive(500, 500)
+	k.msleep(850)
+	ihs_bindings.encoder_turn_degrees_v2(200, -20)
+	# Drive to mid line 
+	drive_to_line(-100, -100, left_side, right_side)
+	align_start_time = time()
+	drive_to_line_white(-100, -100, left_side, right_side)
+    # Goes to middle of mid line
+	drive(100, 100)
+	k.msleep((time() - align_start_time) / 2)
+	drive(0, 0)
+	move_servo_slowly(ARM_PORT, ARM_DOWN, 5)
+	move_servo_slowly(CLAW_PORT, CLAW_OPEN, 5)
+	ihs_bindings.encoder_turn_degrees_v2(200, 80)
+	sweeper_align_black(30, -30)
+	#drive to switch
+	line_follow(SWEEPER_TOPHAT_PORT, is_right_front_white)
+	drive(0, 0)
+	#flick up
+	move_servo(ARM_PORT, ARM_SWITCH_UP)
 
-	move_servo_slowly(CLAW_PORT, CLAW_DRAWER + 10)
-	ihs_bindings.encoder_turn_degrees_v2(100, -14)
+	k.msleep(500)
+
+	drive(300,300)
+	k.msleep(500)
+	move_servo_slowly(ARM_PORT, ARM_SHELF, 5)
+	#drives towards cubes
+	line_follow(SWEEPER_TOPHAT_PORT, is_right_side_white)
 	'''
-	"""
-	#pick up cubes
+	drive(-200,-200)
+	
+	while right_side() > BLACK:
+		pass
+	drive(0,0)
+	'''
+	drive(100,100)
+	k.msleep(150)
 
-	move_servo(CLAW_PORT, CLAW_CLOSED)
+
+    
+    #pick up cubes
+	drive(200,200)
+	k.msleep(100)
+
+
+
+	drive(0,0)
+	#move_servo_slowly(ARM_PORT,ARM_SHELF-10,10)
+	k.msleep(100)
+	move_servo_slowly(CLAW_PORT, CLAW_CLOSED, 10)
 	k.msleep(500)
 	move_servo_slowly(ARM_PORT, ARM_STRAIGHT_UP, 5)
+	
 	#drops off cubes
 	ihs_bindings.encoder_turn_degrees_v2(500, -135)
+	move_servo_slowly(ARM_PORT, ARM_DOWN,5)
 	move_servo(CLAW_PORT, CLAW_OPEN)
 	ihs_bindings.encoder_turn_degrees_v2(500, 130)
-	"""
-	#drive(-500,-500)
-	"""	
+	move_servo_slowly(ARM_PORT, ARM_STRAIGHT_UP, 5)
+	line_follow(SWEEPER_TOPHAT_PORT, is_rod_black)
+	line_follow(SWEEPER_TOPHAT_PORT, is_rod_white)
+	print("done")
+    
+	
+	'''
 	#DO NOT DELETE
 	#move_servo(ROD_PORT,ROD_STRAIGHT)
 	ihs_bindings.encoder_turn_degrees_v2(500,80)
 	k.msleep(100)
-	
+	drive_to_line(200,200,left_side, right_side)
 	#turns toward switch
 	sweeper_align_black(30, -30)
 	sweep()
@@ -274,7 +348,8 @@ def main():
 	drive(0, 0)
 	sweep()
 	"""
-	print("Time:", time.time() - start_time)
+	'''
+	print("Time:", time() - start_time)
 	print("Roomba Battery Used", str((k.get_create_battery_charge() - start_charge)/k.get_create_battery_capacity() * 100) + "%")
 	#move_servo_slowly(ARM_PORT, ARM_LAVA_RESET, 5)
 	
