f6d6745c876e10ee1a1e7cb85453ffc065195ff7
Wes Le <88017350+basedIdiot@users.noreply.github.com>
2024-04-19 19:05:01 -0700

    Update purple_noodle.py

--- big bot/purple_noodle.py
+++ big bot/purple_noodle.py
@@ -62,21 +62,48 @@ def sweep():
 	k.mav(SWEEPER_PORT, -speed)
 	k.msleep(600)
 	k.mav(SWEEPER_PORT, 0)
-def main():
-	"""
+
+def old_main():
+	drive(-200,-200)
+	
 	while right_side() > BLACK:
-		drive(-200,-200)
-		print(right_side())
+		pass
 	drive(0,0)
-	"""
+	move_servo_slowly(ARM_PORT, ARM_SWITCH_UP,10)
+	
+	drive(100,100)
+	k.msleep(150)
+    
+    #pick up cubes
+	drive(200,200)
+	k.msleep(100)
+
+	drive(0,0)
+	move_servo_slowly(ARM_PORT,ARM_SHELF-10,10)
+	k.msleep(100)
+	move_servo_slowly(CLAW_PORT, CLAW_CLOSED, 10)
+	k.msleep(500)
+	move_servo_slowly(ARM_PORT, ARM_STRAIGHT_UP, 5)
+	
+	#drops off cubes
+	ihs_bindings.encoder_turn_degrees_v2(500, -135)
+	move_servo_slowly(ARM_PORT, ARM_DOWN,5)
+	move_servo(CLAW_PORT, CLAW_OPEN)
+	ihs_bindings.encoder_turn_degrees_v2(500, 130)
+	move_servo_slowly(ARM_PORT, ARM_STRAIGHT_UP, 5)
+	line_follow(SWEEPER_TOPHAT_PORT, is_rod_black)
+	line_follow(SWEEPER_TOPHAT_PORT, is_rod_white)
+	print("done")
+
+def main():
 	if not print_battery_info():
 		print("invalid battery info probably means bot will do something dumb")
 		return -1
 	start_charge = k.get_create_battery_charge()
-	#line up arm with free-standing structure with middle rods
+	
+    #STARTING BOX MANUVEURS
 	move_servo_slowly(ARM_PORT, ARM_STRAIGHT_UP, 3) 
 	k.msleep(200)
-	
 	drive(-100, -100)
 	k.msleep(500)
 	drive(0, 0)
@@ -85,6 +112,7 @@ def main():
 	drive_to_line(250, 250, left_side, right_side)
 	#drive_to_line_white(300, 300, left_side, right_side)
 	k.msleep(100)
+    #END OF STARTING BOX MANUVAUERS
 
 	start_time = time()
 	drive(500, 500) #orig speed: 150, 150
@@ -92,13 +120,11 @@ def main():
 	ihs_bindings.encoder_turn_degrees_v2(500, -90) #orig speed: 50, 90 deg clockwise
 
 	#go to middle line
-	#ihs_bindings.encoder_turn_degrees_v2(100, -5)
-
 	drive_to_line(250, 250, left_side, right_side)
 	drive(300,300)
 	k.msleep(100)
-	
 	ihs_bindings.encoder_turn_degrees_v2(500,90)
+
 	#sweep()
 	drive_to_line(300, 300, left_side, right_side)
 	drive_to_line_white(150,150, left_side, right_side)
@@ -120,100 +146,70 @@ def main():
 
 	line_follow(SWEEPER_TOPHAT_PORT, is_rod_black)
 	drive(0,0)
-	'''
-	drive(-200,-200)
-	
-	while right_side() > BLACK:
-		pass
-	drive(0,0)
-	move_servo_slowly(ARM_PORT, ARM_SWITCH_UP,10)
-	
-	drive(100,100)
-	k.msleep(150)
-
-
-    
-    #pick up cubes
-	drive(200,200)
-	k.msleep(100)
-
-
-
-	drive(0,0)
-	move_servo_slowly(ARM_PORT,ARM_SHELF-10,10)
-	k.msleep(100)
-	move_servo_slowly(CLAW_PORT, CLAW_CLOSED, 10)
-	k.msleep(500)
-	move_servo_slowly(ARM_PORT, ARM_STRAIGHT_UP, 5)
-	
-	#drops off cubes
-	ihs_bindings.encoder_turn_degrees_v2(500, -135)
-	move_servo_slowly(ARM_PORT, ARM_DOWN,5)
-	move_servo(CLAW_PORT, CLAW_OPEN)
-	ihs_bindings.encoder_turn_degrees_v2(500, 130)
-	move_servo_slowly(ARM_PORT, ARM_STRAIGHT_UP, 5)
-	line_follow(SWEEPER_TOPHAT_PORT, is_rod_black)
-	line_follow(SWEEPER_TOPHAT_PORT, is_rod_white)
-	print("done")
-	'''
 
 	#turn towards drawer
 	
-	ihs_bindings.encoder_turn_degrees_v2(500, -33)
-	
+	ihs_bindings.encoder_turn_degrees_v2(500, -30)
+	rod_align_black(-30, 30)
+	rod_align_white(30, -30)
 	move_servo(CLAW_PORT, 100)
 
-	move_servo_slowly(ARM_PORT, ARM_DOWN - 20, 5)
+	move_servo_slowly(ARM_PORT, ARM_DOWN, 5)
 
 	k.msleep(500)
-
-
-	move_servo(CLAW_PORT, CLAW_DRAWER)
+	
 	#drive into handle
-	drive(100,100)
+
+	drive(50, 50)
 	while right_side() < BLACK:
 		pass
-	k.msleep(200)#0g: 170
+	while right_side() > BLACK:
+		pass
+	#k.msleep(400)#0g: 170
+	"""
 	drive(0, 0)
 	#re adjust
 	ihs_bindings.encoder_turn_degrees_v2(100, 3)
 	ihs_bindings.encoder_turn_degrees_v2(100, -2)
-	
+	"""
+	drive(0, 0)
+	move_servo(CLAW_PORT, CLAW_DRAWER)
+	drive(-100, -100)
+	k.msleep(1200)
+	drive(0, 0)
 
-	#k.msleep(1000)
-	ihs_bindings.encoder_turn_degrees_v2(50, -3)
-	drive(25, 25)
+	ihs_bindings.encoder_turn_degrees_v2(30,-5)
+	ihs_bindings.encoder_turn_degrees_v2(30,5)
+
+	move_servo(CLAW_PORT, CLAW_DRAWER - 30)
+	ihs_bindings.encoder_turn_degrees_v2(50, 3)
+
+	#pull drawer out
+	move_servo(ROD_PORT,ROD_SIDE - 80)
+	k.msleep(500) #DO NOT DELETE necessary for rod align 
+	rod_align_black(-20, 20) #changed from 20
+	drive(20, -20)
 	k.msleep(100)
+	#ihs_bindings.encoder_turn_degrees_v2(20, -5) #possibly 9 try next run
 	drive(0, 0)
-	#k.msleep(1000)
-	#re adjust
-	move_servo(CLAW_PORT, CLAW_DRAWER + 15)
-	ihs_bindings.encoder_turn_degrees_v2(50, -10)
-	ihs_bindings.encoder_turn_degrees_v2(50, 5)
-	drive(50, 50)
-	k.msleep(100)
-	ihs_bindings.encoder_turn_degrees_v2(50, -10)
-	ihs_bindings.encoder_turn_degrees_v2(50, 5)
-	drive(50, 50)
-	k.msleep(100)
-	ihs_bindings.encoder_turn_degrees_v2(50, -5)
-	return
-	"""
-	drive(50, 50)
-	k.msleep(100)
-	drive(0, 0)
-	ihs_bindings.encoder_turn_degrees_v2(50, -10)
-	"""
 	#release drawer
-	drive(-200,-200)
-	k.msleep(350)
-	drive(0,0)
-	move_servo_slowly(ARM_PORT,ARM_STRAIGHT_UP, 10)
-	#ihs_bindings.encoder_turn_degrees_v2(100,45)
+	move_servo_slowly(CLAW_PORT, 281, 10) #opens the claw to make the drawer not crooked
+	ihs_bindings.encoder_turn_degrees_v2(20, 2)
+    
+	drive(200, 200)
+	k.msleep(800)
+	drive(0, 0)
+	ihs_bindings.encoder_turn_degrees_v2(50, -10)
+	move_servo_slowly(ARM_PORT, ARM_STRAIGHT_UP, 10)
+
 	#drive back 
 	drive(500, 500)
 	k.msleep(550)
 	ihs_bindings.encoder_turn_degrees_v2(200, -20)
+	return
+	#END OF DRAWER SEQUENCE
+
+	#START OF SWITCH SEQUENCE
 	# Drive to mid line 
 	drive_to_line(-100, -100, left_side, right_side)
 	drive(-500, -500)
@@ -225,11 +221,13 @@ def main():
 	move_servo_slowly(CLAW_PORT, CLAW_OPEN, 5)
 	ihs_bindings.encoder_turn_degrees_v2(200, 80)
 	sweeper_align_black(30, -30)
+
 	#drive to switch
 	drive(0, 0)
 	k.msleep(500)
 	line_follow(SWEEPER_TOPHAT_PORT, is_right_front_white)
 	drive(0, 0)
+
 	#flick up
 	move_servo(ARM_PORT, ARM_SWITCH_UP)
 
@@ -272,7 +270,7 @@ def main():
 	ihs_bindings.encoder_turn_degrees_v2(500, 130)
 	move_servo_slowly(ARM_PORT, ARM_STRAIGHT_UP, 5)
 	line_follow(SWEEPER_TOPHAT_PORT, is_rod_black)
-	line_follow(SWEEPER_TOPHAT_PORT, is_rod_white)\
+	line_follow(SWEEPER_TOPHAT_PORT, is_rod_white)
 	#print("done")
     
 	
@@ -382,6 +380,7 @@ if __name__ == "__main__":
 	#drive(0,0)
 	#drive_to_line_white(150,150, left_side, right_side)
 	main()
+	
 
 
 	#ihs_bindings.encoder_turn_degrees_v2(100,180)
