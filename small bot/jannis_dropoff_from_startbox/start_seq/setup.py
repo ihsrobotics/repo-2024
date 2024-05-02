222ea3984284beeefc892fbcb50a048607d39b69
daylin2 <122577630+daylin2@users.noreply.github.com>
2024-05-01 19:05:49 -0700

    Update setup.py

--- small bot/jannis_dropoff_from_startbox/start_seq/setup.py
+++ small bot/jannis_dropoff_from_startbox/start_seq/setup.py
@@ -4,13 +4,15 @@ from bot_functions import *
 from config_loader import *
 
 def setup():
-    # set_arm_pos(650)
-    k.set_servo_position(CLAW_SERVO, 1000)
+    k.set_servo_position(CLAW_SERVO, CLAW_COMB_CLOSE_POS)
     k.msleep(100)
-    k.set_servo_position(JANNIS_SERVO, 830)
+    k.set_servo_position(JANNIS_SERVO, JANNIS_DOWN_POS)
     k.msleep(100)
-    k.set_servo_position(BOOM_SERVO, 1600)
+    k.set_servo_position(BOOM_SERVO, 175)
     k.msleep(100)
+    while k.analog(SLIDE) > ARM_UP_POS:
+        k.mav(ARM, -1500)
+    stop_motor(ARM)
 
 if __name__ == "__main__":
     k.enable_servos()
